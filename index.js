02b3050ab8dfe25779eea1b8ddf27a8681e98926
Elijah Oquendo <elijahoquendo420@gmail.com>
2023-11-19 12:33:48 -0500

    class extension lab

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Your code here
+class Polygon {
+    constructor(sides) {
+      this.sides = sides;
+    }
+  
+    get countSides() {
+      return this.sides.length;
+    }
+  
+    get perimeter() {
+      return this.sides.reduce((sum, side) => sum + side, 0);
+    }
+  }
+
+  class Triangle extends Polygon {
+    constructor(sides) {
+      super(sides);
+    }
+  
+    get isValid() {
+      const [a, b, c] = this.sides;
+      return a + b > c && a + c > b && b + c > a;
+    }
+  }
+  class Square extends Polygon {
+    constructor(sides) {
+      super(sides);
+    }
+  
+    get isValid() {
+      const [a, b, c, d] = this.sides;
+      return a === b && b === c && c === d;
+    }
+  
+    get area() {
+      const side = this.sides[0];
+      return side * side;
+    }
+  }
